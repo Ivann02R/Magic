dcd0f3ae8c03d51699b9fa553587e08f32915f31
Ivann <vanrodri9@gmail.com>
2025-03-23 12:14:21 +0100

    Fix()

--- script.js
+++ script.js
@@ -7,41 +7,52 @@ function toggleMenu() {
     }
 }
 
+let isLoading = false;
+
 function mostrarImagen() {
-    const seleccion = document.getElementById('posicion').value;
-    let opciones = [];
+    
+    if (!isLoading) {
+isLoading = true;
+       
+        const seleccion = document.getElementById('posicion').value;
+        let opciones = [];
 
-    if (seleccion === "1") {
-        opciones = [1, 1, 1, 2, 2, 2, 3, 5, 6]; // Más probabilidad para la imagen 1
-    } else if (seleccion === "2"){
-        opciones = [3, 3, 4, 4, 5, 6, 6, 7, 8]; 
+        if (seleccion === "1") {
+            opciones = [1, 1, 1, 2, 2, 2, 3, 5, 6]; // Más probabilidad para la imagen 1
+        } else if (seleccion === "2") {
+            opciones = [3, 3, 4, 4, 5, 6, 6, 7, 8];
+        }
+        else if (seleccion === "3") {
+            opciones = [4, 4, 6, 6, 7, 8, 9, 10, 10, 12];
+        }
+        else if (seleccion === "4") {
+            opciones = [7, 8, 9, 10, 10, 11, 12, 13, 14];
+        }
+
+        const imagenSeleccionada = opciones[Math.floor(Math.random() * opciones.length)];
+        const imagen = document.getElementById('imagen');
+        imagen.src = `images/${imagenSeleccionada}.png`;
+        document.getElementById('imagen-container').style.display = 'block';
+
+        imagen.classList.remove('tambaleando'); // Quita la clase para reiniciar animación
+        setTimeout(() => {
+            imagen.classList.add('tambaleando'); // Vuelve a agregar la clase tras un pequeño delay
+        }, 10);
+
+        mostrarImagenYContar();
+
+        isLoading = false;
+
     }
-    else if (seleccion === "3"){
-        opciones = [4, 4, 6, 6, 7, 8 ,9, 10, 10, 12]; 
-    }
-    else if (seleccion === "4"){
-        opciones = [7, 8, 9 ,10, 10, 11, 12, 13, 14]; 
-    }
-
-    const imagenSeleccionada = opciones[Math.floor(Math.random() * opciones.length)];
-    const imagen = document.getElementById('imagen');
-    imagen.src = `images/${imagenSeleccionada}.png`;
-    document.getElementById('imagen-container').style.display = 'block';
-
-    imagen.classList.remove('tambaleando'); // Quita la clase para reiniciar animación
-    setTimeout(() => {
-        imagen.classList.add('tambaleando'); // Vuelve a agregar la clase tras un pequeño delay
-    }, 10);
-
 
 
 }
 
 let contador = 1;
 
-    function mostrarImagenYContar() {
-        
-        // Incrementar el contador
-        contador++;
-        document.getElementById("contador").innerText = contador;
-    }
+function mostrarImagenYContar() {
+
+    // Incrementar el contador
+    contador++;
+    document.getElementById("contador").innerText = contador;
+}
